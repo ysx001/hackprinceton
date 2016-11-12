867ae794be264ad770487577849684e620261636
ysx001 <ysx001@bucknell.edu>
2016-11-12 15:00:29 -0500

    Text Extract

--- text_exact.py
+++ text_exact.py
@@ -8,9 +8,8 @@ def text_extract():
     result_1 = pv.read_file(fd)
     result_2 = pe.read_file(fd)
     tags = result_1['description']['tags']
-    emo = result_2[0]['scores'].keys()[0:2]
-    
-    print(emo)
+    emo = result_2[0]['scores'].keys()
+    emo = emo[0:2]
 
     wsj = nltk.corpus.treebank.tagged_words(tagset='universal')
     word_tag_fd = nltk.FreqDist(wsj)
@@ -22,15 +21,10 @@ def text_extract():
     
     verbs = [word for word in tags if (word in verb_list)]
     nouns = [word for word in tags if (word in noun_list)]
-    adjs = [word for word in tags if (word in adj_list)]
+    adjs = [word for word in tags if (word in adj_list)] + emo
     advs = [word for word in tags if (word in adv_list)]
-    print("Verbs:")
-    print(verbs)
-    print("Nouns:")
-    print(nouns)
-    print("ADJS:")
-    print(adjs)
-    print("ADVS")
-    print(advs)
 
-text_extract()
+    return verbs, nouns, adjs, advs
+
+verbs, nouns, adjs, advs = text_extract()
+print(verbs)
